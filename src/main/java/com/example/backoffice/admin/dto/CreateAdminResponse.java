d465e326f70ec6a191c8ec81d615f940cf3b3bd2
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-01-14 12:38:07 +0900

    feat : 관리자 생성 응답 dto 추가

--- src/main/java/com/example/backoffice/admin/dto/CreateAdminResponse.java
+++ src/main/java/com/example/backoffice/admin/dto/CreateAdminResponse.java
@@ -1,4 +1,26 @@
 package com.example.backoffice.admin.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CreateAdminResponse {
+    private final String code;
+    private final String message;
+    private final String name;
+    private final String email;
+    private final String phone;
+    private final String role;
+    private final String status;
+    private final String createdAt;
+
+    public CreateAdminResponse(String code, String message, String name, String email, String phone, String role, String status, String createdAt) {
+        this.code = code;
+        this.message = message;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
 }
