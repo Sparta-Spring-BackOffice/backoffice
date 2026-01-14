3c750846ad2997d7f407f67ad374688538b9edc8
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-01-14 13:07:42 +0900

    fix : CreateAdminResponse에서 code 응답하지 않도록 수정

--- src/main/java/com/example/backoffice/admin/dto/CreateAdminResponse.java
+++ src/main/java/com/example/backoffice/admin/dto/CreateAdminResponse.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 
 @Getter
 public class CreateAdminResponse {
-    private final String code;
     private final String message;
     private final String name;
     private final String email;
@@ -16,7 +15,6 @@ public class CreateAdminResponse {
     private final LocalDateTime createdAt;
 
     public CreateAdminResponse(String code, String message, String name, String email, String phone, String role, String status, LocalDateTime createdAt) {
-        this.code = code;
         this.message = message;
         this.name = name;
         this.email = email;
