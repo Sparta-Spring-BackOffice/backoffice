286d1503854ec1d16d63657701ce0ddb9a7bcee0
youzting <158013348+youzting@users.noreply.github.com>
2026-01-14 20:29:05 +0900

    오류fix : ì오류 코드 수ì 정 로그인에ì러 클래스명 변경

--- src/main/java/com/example/backoffice/authentification/exception/AuthErrorCode.java
+++ src/main/java/com/example/backoffice/authentification/exception/AuthErrorCode.java
@@ -9,7 +9,8 @@ public enum AuthErrorCode {
     LOGIN_PENDING_ERROR("LOGIN_PENDING_ERROR", "계정이 승인 대기 중입니다."),
     LOGIN_DENIED_ERROR("LOGIN_DENIED_ERROR", "승인이 거부되었습니다."),
     LOGIN_SUSPENDED_ERROR("LOGIN_SUSPENDED_ERROR", "정지된 계정입니다."),
-    LOGIN_NON_ACTIVE_ERROR("LOGIN_NON_ACTIVE_ERROR", "비활성화된 계정입니다.");
+    LOGIN_NON_ACTIVE_ERROR("LOGIN_NON_ACTIVE_ERROR", "비활성화된 계정입니다."),
+    LOGIN_ERROR("LOGIN_ERROR", "비밀번호 또는 이메일이 불일치합니다.");
 
     private final String code;
     private final String message;
--- src/main/java/com/example/backoffice/authentification/service/AuthentificationService.java
+++ src/main/java/com/example/backoffice/authentification/service/AuthentificationService.java
@@ -2,15 +2,13 @@ package com.example.backoffice.authentification.service;
 
 import com.example.backoffice.admin.consts.DeclineReason;
 import com.example.backoffice.admin.entity.Administrator;
-import com.example.backoffice.admin.exception.AdminNotFoundException;
 import com.example.backoffice.admin.repository.AdminRepository;
 import com.example.backoffice.authentification.consts.AuthStatus;
 import com.example.backoffice.authentification.dto.LoginRequest;
 import com.example.backoffice.authentification.dto.LoginResponse;
 import com.example.backoffice.authentification.exception.AuthErrorCode;
-import com.example.backoffice.authentification.exception.LoginStatusException;
+import com.example.backoffice.authentification.exception.LoginFailException;
 import com.example.backoffice.common.config.PasswordEncoder;
-import com.example.backoffice.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,20 +24,20 @@ public class AuthentificationService {
     @Transactional
     public LoginResponse login(LoginRequest request) {
         Administrator admin = adminRepository.findByEmail(request.getEmail()).orElseThrow(
-                () -> new AdminNotFoundException(ErrorCode.DUPLICATE_EMAIL)//에러 코드 추후 수정(이메일 불일치)
+                () -> new LoginFailException(AuthErrorCode.LOGIN_ERROR)
         );
         if(!pe.matches(request.getPassword(), admin.getPassword())){
-            throw new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND);//에러 코드 추후 수정(비밀번호 불일치)
+            throw new LoginFailException(AuthErrorCode.LOGIN_ERROR);
         }
         //AdminStatus
         switch (admin.getStatus()) {
             case ACTIVE -> { /* OK */ }
-            case PENDING -> throw new LoginStatusException(AuthErrorCode.LOGIN_PENDING_ERROR);
+            case PENDING -> throw new LoginFailException(AuthErrorCode.LOGIN_PENDING_ERROR);
             case DENIED -> {
-                throw new LoginStatusException(AuthErrorCode.LOGIN_PENDING_ERROR);//사유 추가 해야함
+                throw new LoginFailException(AuthErrorCode.LOGIN_DENIED_ERROR);//사유 추가 해야함
             }
-            case SUSPENDED -> throw new LoginStatusException(AuthErrorCode.LOGIN_SUSPENDED_ERROR);
-            case NON_ACTIVE -> throw new LoginStatusException(AuthErrorCode.LOGIN_NON_ACTIVE_ERROR);
+            case SUSPENDED -> throw new LoginFailException(AuthErrorCode.LOGIN_SUSPENDED_ERROR);
+            case NON_ACTIVE -> throw new LoginFailException(AuthErrorCode.LOGIN_NON_ACTIVE_ERROR);
         }
 
 
