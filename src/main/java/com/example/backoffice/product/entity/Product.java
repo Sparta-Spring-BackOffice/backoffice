9c43d1c71791a03b1705c83ae631854a3538af83
bjw446 <bjw446@naver.com>
2026-01-14 11:27:50 +0900

    상품 등록 추가

--- src/main/java/com/example/backoffice/product/entity/Product.java
+++ src/main/java/com/example/backoffice/product/entity/Product.java
@@ -1,16 +1,16 @@
 package com.example.backoffice.product.entity;
 
+import com.example.backoffice.common.config.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-//
- //
+
 @Getter
 @Entity
 @Table(name = "products")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Product {
+public class Product extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
