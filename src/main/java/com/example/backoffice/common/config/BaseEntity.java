2accd2d53e379adb8fff62dedcc42f7e6ffdc769
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-01-14 12:39:23 +0900

    fix : BaseEntity에서 수정일 반영

--- src/main/java/com/example/backoffice/common/config/BaseEntity.java
+++ src/main/java/com/example/backoffice/common/config/BaseEntity.java
@@ -18,7 +18,7 @@ public abstract class BaseEntity {
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
-//    @LastModifiedDate
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private LocalDateTime modifiedAt;
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
 }
