629aa67426644b0c237fe78860e8b489bee6ab70
bjw446 <bjw446@naver.com>
2026-01-15 16:24:10 +0900

    feat : Validation 추가

--- src/main/java/com/example/backoffice/product/dto/UpdateProductInfoRequest.java
+++ src/main/java/com/example/backoffice/product/dto/UpdateProductInfoRequest.java
@@ -1,13 +1,20 @@
 package com.example.backoffice.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class UpdateProductInfoRequest {
+
+    @NotBlank(message = "상품 이름은 필수 입력 항목입니다.")
     private String name;
+
+    @NotBlank(message = "카테고리는 필수 입력 항목입니다.")
     private String category;
+
+    @NotBlank(message = "가격은 필수 입력 항목입니다.")
     private BigDecimal price;
 
 }
