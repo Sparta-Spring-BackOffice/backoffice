211dd97417001e433111dbcf9a609ac11eb20a8b
youzting <158013348+youzting@users.noreply.github.com>
2026-01-15 19:59:37 +0900

    feat : 리뷰 생성

--- src/main/java/com/example/backoffice/review/controller/ReviewController.java
+++ src/main/java/com/example/backoffice/review/controller/ReviewController.java
@@ -3,26 +3,25 @@ package com.example.backoffice.review.controller;
 import com.example.backoffice.authentification.dto.SessionAdmin;
 import com.example.backoffice.order.dto.CreateOrderResponse;
 import com.example.backoffice.review.dto.CreateReviewRequest;
+import com.example.backoffice.review.dto.CreateReviewResponse;
 import com.example.backoffice.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @PostMapping("/reviews")//리뷰는 어드민에서 사용하는 곳이 아니니깐 admin 사용안함
-    public ResponseEntity<CreateOrderResponse> create(
+    @PostMapping("/orders/{orderId}/products/{productId}/reviews")//리뷰는 어드민에서 사용하는 곳이 아니라서 admin 사용안함
+    public ResponseEntity<CreateReviewResponse> create(
+            @PathVariable Long orderId, Long productId,
            @Valid @RequestBody CreateReviewRequest request,
             @SessionAttribute(name = "loginUser", required = false) SessionAdmin loginAdmin
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(reviewService.save(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewService.save(request,orderId, productId));
     }
 }
