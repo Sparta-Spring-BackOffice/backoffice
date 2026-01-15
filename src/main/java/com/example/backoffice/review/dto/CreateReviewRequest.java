2ae540456aed9b87c677189a34648d86695a6bdf
youzting <158013348+youzting@users.noreply.github.com>
2026-01-15 20:04:15 +0900

    vaild 수정

--- src/main/java/com/example/backoffice/review/dto/CreateReviewRequest.java
+++ src/main/java/com/example/backoffice/review/dto/CreateReviewRequest.java
@@ -2,12 +2,16 @@ package com.example.backoffice.review.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CreateReviewRequest {
+
+    @NotNull(message = "평점은 필수입니다.")
     @Min(value = 1,message = "평점은 1 이상이여야 합니다.")
-    @Max(value = 5, message = "평점은 5 이상이여야 합니다.")
+    @Max(value = 5, message = "평점은 5 이하이여야 합니다.")
     private Integer rating;
+
     private String content;
 }
