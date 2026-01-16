b60d4ad114f91064fa9f8394446fc7fda723f85e
hyu1335 <hyuham1335@gmail.com>
2026-01-16 20:50:59 +0900

    refactor: 성공 코드 추가

--- src/main/java/com/example/backoffice/common/responsecode/SuccessCode.java
+++ src/main/java/com/example/backoffice/common/responsecode/SuccessCode.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatusCode;
 @AllArgsConstructor
 public enum SuccessCode {
     LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공하였습니다."),
-    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃에 성공하였습니다.");
+    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃에 성공하였습니다."),
+    CREATE_SUCCESS(HttpStatus.CREATED,"데이터 생성에 성공하였습니다"),
+    READ_SUCCESS(HttpStatus.OK,"데이터 조회에 성공하였습니다"),
+    UPDATE_SUCCESS(HttpStatus.OK,"데이터 수정에 성공하였습니다"),
+    DELETE_SUCCESS(HttpStatus.OK,"데이터 삭제에 성공하였습니다");
 
     private final HttpStatusCode statusCode;
     private final String message;
