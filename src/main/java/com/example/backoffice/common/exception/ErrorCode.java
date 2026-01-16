b0c39b606647b37b9caa06fe7e94e59225c908b4
bjw446 <bjw446@naver.com>
2026-01-16 12:44:52 +0900

    feat : 주문 전용 에러 코드 추가

--- src/main/java/com/example/backoffice/common/exception/ErrorCode.java
+++ src/main/java/com/example/backoffice/common/exception/ErrorCode.java
@@ -13,8 +13,12 @@ public enum ErrorCode {
     ADMIN_NOT_FOUND("ADMIN_NOT_FOUND", "관리자가 존재하지 않습니다."),
     INSUFFICIENT_ADMIN_ROLE ("INSUFFICIENT_ADMIN_ROLE", "필요한 관리자 역할이 없습니다"),
     PASSWORD_CONFIRM_MISMATCH_ERROR("PASSWORD_CONFIRM_MISMATCH_ERROR", "새 비밀번호가 일치하지 않습니다."),
-    //오타 수정
-    NO_SUCH_PRODUCT("NO_SUCH_PRODUCT", "존재하지 않는 상품입니다.");
+    NO_SUCH_PRODUCT("NO_SUCH_PRODUCT", "존재하지 않는 상품입니다."),
+    NOT_AVAILABLE("NOT_AVAILABLE", "단종된 상품은 주문할 수 없습니다."),
+    OUT_OF_STOCK("OUT_OF_STOCK", "품절된 상품은 주문할 수 없습니다."),
+    INSUFFICIENT_STOCK("INSUFFICIENT_STOCK", "재고가 주문 수량보다 부족하여 주문할 수 없습니다."),
+    NO_SUCH_ORDER("NO_SUCH_ORDER", "존재하지 않는 주문입니다."),
+    UNABLE_CANCEL("UNABLE_CANCEL", "배송중 또는 배송완료 주문은 취소할 수 없습니다.");
     private final String code;
     private final String message;
 }
---
+++ src/main/java/com/example/backoffice/order/exception/InsufficientStockException.java
@@ -0,0 +1,10 @@
+package com.example.backoffice.order.exception;
+
+import com.example.backoffice.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public class InsufficientStockException extends OrderException{
+    public InsufficientStockException(ErrorCode errorCode) {
+        super(HttpStatus.BAD_REQUEST, errorCode);
+    }
+}
