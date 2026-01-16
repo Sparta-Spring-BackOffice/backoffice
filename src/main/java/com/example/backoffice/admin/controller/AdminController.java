b70acfc3a83a022f0df620bf6af890301510b63f
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-01-16 15:52:51 +0900

    feat : 관리자 단건 조회 시 슈퍼 관리자 검증

--- src/main/java/com/example/backoffice/admin/controller/AdminController.java
+++ src/main/java/com/example/backoffice/admin/controller/AdminController.java
@@ -79,8 +79,13 @@ public class AdminController {
     }
     //관리자 단건조회(조건 : 로그인, 권한 수준 : 모든 관리자)
     @GetMapping("/admin/administrators/{administratorId}")
-    public ResponseEntity<GetOneAdminResponse> getOneAdmin(@PathVariable Long administratorId) {
-        return ResponseEntity.status(HttpStatus.OK).body(adminService.getOneAdmin(administratorId));
+    public ResponseEntity<GetOneAdminResponse> getOneAdmin(
+            @SessionAttribute(name = "loginUser", required = false) SessionAdmin loginAdmin,
+            @PathVariable Long administratorId) {
+        if(loginAdmin == null){
+            throw new UnauthorizedException(AuthErrorCode.NOT_LOGIN);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(adminService.getOneAdmin(loginAdmin.getId(), administratorId));
     }
     //관리자 거부(조건 : 로그인, 권한 수준 : 슈퍼 관리자)
     @PutMapping("/admin/administrators/{administratorId}/denial")
--- src/main/java/com/example/backoffice/admin/service/AdminService.java
+++ src/main/java/com/example/backoffice/admin/service/AdminService.java
@@ -61,6 +61,26 @@ public class AdminService {
                 )).toList();
     }
 
+    @Transactional(readOnly = true)
+    public GetOneAdminResponse getOneAdmin(Long loginId, Long administratorId) {
+        //슈퍼 관리자인지 검사
+        checkSuperAdmin(loginId);
+        Administrator administrator = adminRepository.findById(administratorId).orElseThrow(
+                () -> new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND)
+        );
+
+        return new GetOneAdminResponse(
+                administrator.getName(),
+                administrator.getEmail(),
+                administrator.getPhone(),
+                administrator.getRole().getRoleName(),
+                administrator.getStatus().getStatusName(),
+                administrator.getCreatedAt(),
+                administrator.getModifiedAt(),
+                administrator.getApprovedAt()
+        );
+    }
+
     @Transactional
     public CreateAdminResponse create(CreateAdminRequest request) {
         //이메일 중복 확인
@@ -85,24 +105,6 @@ public class AdminService {
                 admin.getRole().getRoleName(),
                 admin.getStatus().getStatusName(),
                 admin.getCreatedAt()
-        );
-    }
-
-    @Transactional(readOnly = true)
-    public GetOneAdminResponse getOneAdmin(Long administratorId) {
-        Administrator administrator = adminRepository.findById(administratorId).orElseThrow(
-                () -> new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND)
-        );
-
-        return new GetOneAdminResponse(
-                administrator.getName(),
-                administrator.getEmail(),
-                administrator.getPhone(),
-                administrator.getRole().getRoleName(),
-                administrator.getStatus().getStatusName(),
-                administrator.getCreatedAt(),
-                administrator.getModifiedAt(),
-                administrator.getApprovedAt()
         );
     }
 
@@ -189,22 +191,7 @@ public class AdminService {
                 administrator.getModifiedAt()
         );
     }
-    //(공통기능)슈퍼 관리자인지 검사
-    private void checkSuperAdmin(Long loginId) {
-        Administrator loginAdmin = adminRepository.findById(loginId).orElseThrow(
-                () -> new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND)
-        );
-        if (loginAdmin.getRole() != AdminRole.SUPER_ADMIN) {
-            throw new InsufficientRoleException(ErrorCode.INSUFFICIENT_ADMIN_ROLE);
-        }
-    }
-    //(공통기능)존재하는 관리자인지 검사
-    private Administrator FindAndGet(Long targetId) {
-        Administrator targetAdmin = adminRepository.findById(targetId).orElseThrow(
-                () -> new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND)
-        );
-        return targetAdmin;
-    }
+
     //activateAdmin, denyAdmin과 동일
     @Transactional
     public void suspendAdmin(Long loginId, Long targetId) {
@@ -215,6 +202,7 @@ public class AdminService {
         //상태 변경
         targetAdmin.suspend();
     }
+
     @Transactional
     public void deactivateAdmin(Long loginId, Long targetId) {
         //슈퍼 관리자인지 검사
@@ -224,6 +212,7 @@ public class AdminService {
         //상태 변경
         targetAdmin.deactivate();
     }
+
     @Transactional
     public void deleteAdmin(Long loginId, Long administratorId) {
         //슈퍼 관리자인지 검사
@@ -233,5 +222,23 @@ public class AdminService {
         if(!existence) throw new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND);
         //상태 변경
         adminRepository.deleteById(administratorId);
+    }
+
+    //(공통기능)슈퍼 관리자인지 검사
+    private void checkSuperAdmin(Long loginId) {
+        Administrator loginAdmin = adminRepository.findById(loginId).orElseThrow(
+                () -> new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND)
+        );
+        if (loginAdmin.getRole() != AdminRole.SUPER_ADMIN) {
+            throw new InsufficientRoleException(ErrorCode.INSUFFICIENT_ADMIN_ROLE);
+        }
+    }
+
+    //(공통기능)존재하는 관리자인지 검사
+    private Administrator FindAndGet(Long targetId) {
+        Administrator targetAdmin = adminRepository.findById(targetId).orElseThrow(
+                () -> new AdminNotFoundException(ErrorCode.ADMIN_NOT_FOUND)
+        );
+        return targetAdmin;
     }
 }
