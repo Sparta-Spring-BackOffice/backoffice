6762837e939c4fe734ac660fe26fd514dcedd0c0
hyu1335 <hyuham1335@gmail.com>
2026-01-17 09:58:06 +0900

    fix: 에러 메세지 출력 수정

--- src/main/java/com/example/backoffice/product/exception/ProductException.java
+++ src/main/java/com/example/backoffice/product/exception/ProductException.java
@@ -7,12 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class ProductException extends RuntimeException {
 
-    public HttpStatus httpStatus;
+    public HttpStatus status;
     public ErrorCode errorCode;
 
     public ProductException(HttpStatus httpStatus, ErrorCode errorCode) {
         super(errorCode.getMessage());
-        this.httpStatus = httpStatus;
+        this.status = httpStatus;
         this.errorCode = errorCode;
     }
 }
