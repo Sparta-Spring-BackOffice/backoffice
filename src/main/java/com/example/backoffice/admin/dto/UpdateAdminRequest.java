1b4b0cf8c025598bd0fbc6ad6706ead537f31cb6
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-01-19 16:14:28 +0900

    fix : 관리자 생성, 수정 검증 시 메시지 동기화

--- src/main/java/com/example/backoffice/admin/dto/UpdateAdminRequest.java
+++ src/main/java/com/example/backoffice/admin/dto/UpdateAdminRequest.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 @Getter
 public class UpdateAdminRequest {
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
     private String name;
-    @NotBlank
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     @Email
     private String email;
     @NotBlank
